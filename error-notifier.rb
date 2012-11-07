df8d2c15ef67d7d242dc5ac6397cfa6b37a92c22
Jason R. Clark <jclark@newrelic.com>
2012-11-06 23:04:53 -0800

    Relative or explicit root directory, link to open on notification.

--- error-notifier.rb
+++ error-notifier.rb
@@ -1,13 +1,15 @@
 require 'terminal-notifier'
 require 'file-tail'
 
+$newrelic_root = ARGV[0] || Dir.pwd
+
 AGENT_ERRORS = [
   /(ERROR \: .*)/,
 ]
 
-def tail_file(path, title, patterns)
+def tail_file(path, patterns)
   Thread.new do 
-    File.open(path) do |log|
+    File.open(File.join($newrelic_root, path)) do |log|
       log.extend(File::Tail)
       log.interval = 10
       log.backward(10)
@@ -15,7 +17,9 @@ def tail_file(path, title, patterns)
       log.tail do |line|
         match = patterns.map { |r| r.match(line) }.compact.first
         if !match.nil?
-          TerminalNotifier.notify(normalize_error(match[0]), :title => title) 
+          TerminalNotifier.notify(normalize_error(match[0]),
+                                  :title => path,
+                                  :open => "file://#{path}") 
         end
       end
     end
@@ -28,13 +32,7 @@ end
 
 threads = []
 
-threads << tail_file('/Users/jclark/source/newrelic/rpm_test_app/log/newrelic_agent.log', 
-              'rpm_test_app',
-              AGENT_ERRORS)
-
-threads << tail_file('/Users/jclark/source/newrelic/rpm_test_app/log/newrelic_agent.log', 
-              'RPM_TEST_APP AGAIN!',
-              AGENT_ERRORS)
+threads << tail_file('rpm_test_app/log/newrelic_agent.log', AGENT_ERRORS)
 
 threads.each { |t| t.join }
 
