1958fa52e251e3a3934b718e948f5567db0a07c1
Jason R. Clark <jclark@newrelic.com>
2012-11-07 09:35:13 -0800

    Tracking errors in collector and beacon, along with rpm_test_app

--- error-notifier.rb
+++ error-notifier.rb
@@ -3,13 +3,19 @@ require 'file-tail'
 
 $newrelic_root = ARGV[0] || Dir.pwd
 
-AGENT_ERRORS = [
+RUBY_ERRORS = [
   /(ERROR \: .*)/,
+]
+
+JAVA_ERRORS = [
+  /( FATAL .*)/,
+  /(.*Exception\:.*)/,
 ]
 
 def tail_file(path, patterns)
   Thread.new do 
-    File.open(File.join($newrelic_root, path)) do |log|
+    full_path = File.absolute_path(File.join($newrelic_root, path))
+    File.open(full_path) do |log|
       log.extend(File::Tail)
       log.interval = 10
       log.backward(10)
@@ -19,7 +25,7 @@ def tail_file(path, patterns)
         if !match.nil?
           TerminalNotifier.notify(normalize_error(match[0]),
                                   :title => path,
-                                  :open => "file://#{path}") 
+                                  :open => "file://#{full_path}") 
         end
       end
     end
@@ -32,7 +38,9 @@ end
 
 threads = []
 
-threads << tail_file('rpm_test_app/log/newrelic_agent.log', AGENT_ERRORS)
+threads << tail_file('rpm_test_app/log/newrelic_agent.log', RUBY_ERRORS)
+threads << tail_file('rpm_site/java_collector/log/collector.8081.log', JAVA_ERRORS)
+threads << tail_file('rpm_site/beacon/log/server.9000.log', JAVA_ERRORS)
 
 threads.each { |t| t.join }
 
